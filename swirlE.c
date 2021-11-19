868012278a2fe0ee07dcb1fa1cbb67fd190216f9
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-19 17:44:12 -0500

    correction fact

--- swirlE.c
+++ swirlE.c
@@ -184,6 +184,22 @@ int main() {
             rc_servo_send_pulse_normalized(8, (servo_pos * 0.07 * 2));
             //printf("L: %f\n", pulse);
         }
+        else if (corr_factor_avg - 0.6 > 0) {
+
+            //ch = 7; right servo
+            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.07*1.2));
+            //printf("R: %f\n", pulse);
+            //ch = 8; left servo
+            rc_servo_send_pulse_normalized(8, servo_pos * 0.07);
+        } else if (corr_factor_avg + 0.6 < 0) {
+
+            //ch = 7; right servo
+            rc_servo_send_pulse_normalized(7, -1 * r_wheel_gain * (servo_pos * 0.07));
+
+            //ch = 8; left servo
+            rc_servo_send_pulse_normalized(8, (servo_pos * 0.07 * 1.2));
+            //printf("L: %f\n", pulse);
+        }
         else {
             //ch = 7; right servo
             rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.07));
