c7b80e03847304c38ca4d4d114d4fb6188344234
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-19 17:35:11 -0500

    SPEED

--- swirlE.c
+++ swirlE.c
@@ -121,59 +121,59 @@ int main() {
         else if (corr_factor_avg - 1.0 > 0) {
 
             //ch = 7; right servo
-            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.08*4.5));
+            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.099*4.5));
             //printf("R: %f\n", pulse);
             //ch = 8; left servo
-            rc_servo_send_pulse_normalized(8, servo_pos * 0.08);
+            rc_servo_send_pulse_normalized(8, servo_pos * 0.099);
         } else if (corr_factor_avg + 1.0 < 0) {
 
             //ch = 7; right servo
-            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.08));
+            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.099));
 
             //ch = 8; left servo
-            rc_servo_send_pulse_normalized(8, (servo_pos * 0.08*4.5));
+            rc_servo_send_pulse_normalized(8, (servo_pos * 0.099*4.5));
             //printf("L: %f\n", pulse);
 
         }
         else if (corr_factor_avg - 0.8 > 0) {
 
             //ch = 7; right servo
-            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.08*3.5));
+            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.099*3.5));
             //printf("R: %f\n", pulse);
             //ch = 8; left servo
-            rc_servo_send_pulse_normalized(8, servo_pos * 0.08);
+            rc_servo_send_pulse_normalized(8, servo_pos * 0.099);
         } else if (corr_factor_avg + 0.8 < 0) {
 
             //ch = 7; right servo
-            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.08));
+            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.099));
 
             //ch = 8; left servo
-            rc_servo_send_pulse_normalized(8, (servo_pos * 0.08*3.5));
+            rc_servo_send_pulse_normalized(8, (servo_pos * 0.099*3.5));
             //printf("L: %f\n", pulse);
 
 //        } else if (corr_factor_avg - 0.7 > 0) {
 //
 //            //ch = 7; right servo
-//            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.08*1.5));
+//            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.099*1.5));
 //            //printf("R: %f\n", pulse);
 //            //ch = 8; left servo
-//            rc_servo_send_pulse_normalized(8, servo_pos * 0.08);
+//            rc_servo_send_pulse_normalized(8, servo_pos * 0.099);
 //        } else if (corr_factor_avg + 0.7 < 0) {
 //
 //            //ch = 7; right servo
-//            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.08));
+//            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.099));
 //
 //            //ch = 8; left servo
-//            rc_servo_send_pulse_normalized(8, (servo_pos * 0.08*1.5));
+//            rc_servo_send_pulse_normalized(8, (servo_pos * 0.099*1.5));
 //            //printf("L: %f\n", pulse);
 //
         }
         else {
             //ch = 7; right servo
-            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.08));
+            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.099));
 
             //ch = 8; left servo
-            rc_servo_send_pulse_normalized(8, (servo_pos * 0.08));
+            rc_servo_send_pulse_normalized(8, (servo_pos * 0.099));
         }
 
         // sleep roughly enough to maintain frequency_hz
