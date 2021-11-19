6523bec085025005a3e4951a0a4e3f22e9f378b6
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-19 08:34:54 -0500

    changing ctl loop to multi-step

--- swirlE.c
+++ swirlE.c
@@ -122,23 +122,23 @@ int main() {
             rc_servo_send_pulse_normalized(8, (servo_pos * 0.07*6));
             //printf("L: %f\n", pulse);
 
-        } else if (corr_factor_avg - 0.7 > 0) {
-
-            //ch = 7; right servo
-            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.07*1.5));
-            //printf("R: %f\n", pulse);
-            //ch = 8; left servo
-            rc_servo_send_pulse_normalized(8, servo_pos * 0.07);
-        } else if (corr_factor_avg + 0.7 < 0) {
-
-            //ch = 7; right servo
-            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.07));
-
-            //ch = 8; left servo
-            rc_servo_send_pulse_normalized(8, (servo_pos * 0.07*1.5));
-            //printf("L: %f\n", pulse);
-
-        }
+//        } else if (corr_factor_avg - 0.7 > 0) {
+//
+//            //ch = 7; right servo
+//            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.07*1.5));
+//            //printf("R: %f\n", pulse);
+//            //ch = 8; left servo
+//            rc_servo_send_pulse_normalized(8, servo_pos * 0.07);
+//        } else if (corr_factor_avg + 0.7 < 0) {
+//
+//            //ch = 7; right servo
+//            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.07));
+//
+//            //ch = 8; left servo
+//            rc_servo_send_pulse_normalized(8, (servo_pos * 0.07*1.5));
+//            //printf("L: %f\n", pulse);
+//
+//        }
         else {
             //ch = 7; right servo
             rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.07));
