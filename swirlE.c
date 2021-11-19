56fc66c3f276551229e9ee3472ec0a120165e08e
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-19 04:55:12 -0500

    testing new ctl loop 1

--- swirlE.c
+++ swirlE.c
@@ -57,14 +57,15 @@ int main()
     double max_speed = 10.0;
     double gain = 10;
     double r_wheel_gain = 1.2;
+    int avg_val_ctr = 0;
     while (1) {
         //colour_sensor(CS_OUT1);//left sensor
         //colour_sensor(CS_OUT2);//right sensor
         leftC_sense = colour_sensor_red(CS_OUT1) + colour_sensor_green(CS_OUT1) + colour_sensor_blue(CS_OUT1);
         rightC_sense = colour_sensor_red(CS_OUT2) + colour_sensor_green(CS_OUT2) + colour_sensor_blue(CS_OUT2);
 
-        right_corr_factor = gain*abs(rightC_sense - leftC_sense)/(leftC_sense + rightC_sense);
-        left_corr_factor = gain*abs(leftC_sense - rightC_sense)/(leftC_sense + rightC_sense);
+        corr_factor = gain*abs(rightC_sense - leftC_sense)/(leftC_sense + rightC_sense);
+        //left_corr_factor = gain*abs(leftC_sense - rightC_sense)/(leftC_sense + rightC_sense);
 
         printf("R %f L %f\n", rightC_sense, leftC_sense);
         servo_pos += direction * sweep_limit / frequency_hz;
@@ -73,13 +74,13 @@ int main()
             servo_pos = sweep_limit;
         }
 
-        if (rightC_sense > leftC_sense + 350*10000) {
-            //ch = 7; right servo
-            rc_servo_send_pulse_normalized(7, -servo_pos *7*1/4* r_wheel_gain/max_speed);
-
-            //ch = 8; left servo
-            rc_servo_send_pulse_normalized(8, servo_pos * .5/4/max_speed);
-        }
+//        if (rightC_sense > leftC_sense + 350*10000) {
+//            //ch = 7; right servo
+//            rc_servo_send_pulse_normalized(7, -servo_pos *7*1/4* r_wheel_gain/max_speed);
+//
+//            //ch = 8; left servo
+//            rc_servo_send_pulse_normalized(8, servo_pos * .5/4/max_speed);
+//        }
 //        else if (leftC_sense > rightC_sense + 350) {
 //            //ch = 7; right servo
 //            rc_servo_send_pulse_normalized(7, -servo_pos * .5/4* r_wheel_gain/max_speed);
@@ -116,6 +117,8 @@ int main()
 //            rc_servo_send_pulse_normalized(8, servo_pos * 1 * 1.1/4/max_speed);
 //
 //        }
+        printf("c fact: %f\n",corr_factor);
+        if(corr_factor>10000000){}
         else {
 //            //ch = 7; right servo
 //            rc_servo_send_pulse_normalized(7, -servo_pos * 1/1.0* r_wheel_gain/max_speed);
