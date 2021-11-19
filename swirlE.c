496c394760aac30486336426fc6adbcdaced06f4
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-19 05:49:39 -0500

    testing new ctl loop 11

--- swirlE.c
+++ swirlE.c
@@ -66,7 +66,7 @@ int main()
         leftC_sense = colour_sensor_red(CS_OUT1) + colour_sensor_green(CS_OUT1) + colour_sensor_blue(CS_OUT1);
         rightC_sense = colour_sensor_red(CS_OUT2) + colour_sensor_green(CS_OUT2) + colour_sensor_blue(CS_OUT2);
 
-        corr_factor = gain*abs(rightC_sense - leftC_sense)/(leftC_sense + rightC_sense);
+        corr_factor = gain*(rightC_sense - leftC_sense)/(leftC_sense + rightC_sense);
         //left_corr_factor = gain*abs(leftC_sense - rightC_sense)/(leftC_sense + rightC_sense);
         corr_arr[avg_val_ctr] = corr_factor;
         corr_factor++;
@@ -132,14 +132,14 @@ int main()
 //        }
         printf("%f\n",corr_factor_avg);
         if(false){}
-        else {
+        else if(corr_factor_avg > 0) {
 //            //ch = 7; right servo
 //            rc_servo_send_pulse_normalized(7, -servo_pos * 1/1.0* r_wheel_gain/max_speed);
 //
 //            //ch = 8; left servo
 //            rc_servo_send_pulse_normalized(8, servo_pos * 1/1.0/max_speed);
             //ch = 7; right servo
-            rc_servo_send_pulse_normalized(7, -servo_pos*r_wheel_gain * (1.5*corr_factor_avg + 0.15));
+            rc_servo_send_pulse_normalized(7, -servo_pos*r_wheel_gain * (1.5*abs(corr_factor_avg) + 0.15));
 
             //ch = 8; left servo
             rc_servo_send_pulse_normalized(8, servo_pos * 0.15);
@@ -147,6 +147,21 @@ int main()
         // sleep roughly enough to maintain frequency_hz
         rc_usleep(1000000 / frequency_hz);
     }
+    else if(corr_factor_avg < 0) {
+//            //ch = 7; right servo
+//            rc_servo_send_pulse_normalized(7, -servo_pos * 1/1.0* r_wheel_gain/max_speed);
+//
+//            //ch = 8; left servo
+//            rc_servo_send_pulse_normalized(8, servo_pos * 1/1.0/max_speed);
+        //ch = 7; right servo
+        rc_servo_send_pulse_normalized(7, -servo_pos*r_wheel_gain * 0.15));
+
+        //ch = 8; left servo
+        rc_servo_send_pulse_normalized(8, servo_pos * (1.5*abs(corr_factor_avg) + 0.15));
+    }
+    // sleep roughly enough to maintain frequency_hz
+    rc_usleep(1000000 / frequency_hz);
+}
     // turn off power rail and cleanup
     rc_servo_power_rail_en(0);
     rc_servo_cleanup();
