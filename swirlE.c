038807317299f3a8068013083dab3c61229d0f1d
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-19 08:17:12 -0500

    changing ctl loop to binary step

--- swirlE.c
+++ swirlE.c
@@ -57,7 +57,7 @@ int main() {
     double gain = 10;
     double r_wheel_gain = 1.4;
     int avg_val_ctr = 0;
-    double corr_arr[3];
+    double corr_arr[5];
     //double delta_arr[5];
     double corr_factor_avg = 0.0;
     while (1) {
@@ -77,14 +77,14 @@ int main() {
         corr_arr[avg_val_ctr] = corr_factor;
         avg_val_ctr++;
         //delta_arr[avg_val_ctr] = rightC_sense - leftC_sense;
-        if(avg_val_ctr>=3)
+        if(avg_val_ctr>=5)
         {
             avg_val_ctr = 0;
             double sum = 0;
-            for(int i = 0; i<3; i++)
+            for(int i = 0; i<5; i++)
             {
                 sum += corr_arr[i];
-                corr_factor_avg = sum/3.0;
+                corr_factor_avg = sum/5.0;
             }
         }
         printf("%f\n",corr_factor_avg);
