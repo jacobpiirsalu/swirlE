5b07976c3264a9148621288c8c818ab8374b3735
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-19 18:14:53 -0500

    bug fixes

--- swirlE.c
+++ swirlE.c
@@ -217,6 +217,22 @@ int main() {
             rc_servo_send_pulse_normalized(8, (servo_pos * 0.075 * 3));
             //printf("L: %f\n", pulse);
         }
+        else if (corr_factor_avg - 0.4 > 0) {
+
+            //ch = 7; right servo
+            rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.075*2));
+            //printf("R: %f\n", pulse);
+            //ch = 8; left servo
+            rc_servo_send_pulse_normalized(8, servo_pos * 0.075);
+        } else if (corr_factor_avg + 0.4 < 0) {
+
+            //ch = 7; right servo
+            rc_servo_send_pulse_normalized(7, -1 * r_wheel_gain * (servo_pos * 0.075));
+
+            //ch = 8; left servo
+            rc_servo_send_pulse_normalized(8, (servo_pos * 0.075 * 2));
+            //printf("L: %f\n", pulse);
+        }
         else {
             //ch = 7; right servo
             rc_servo_send_pulse_normalized(7, -1*r_wheel_gain * (servo_pos * 0.075));
