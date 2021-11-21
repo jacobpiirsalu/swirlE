35684b0de5a484cf3188ca3bd1166517d7715843
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-21 02:59:30 -0500

    larger window for testing - working on implementing blue and green sense

--- swirlE.c
+++ swirlE.c
@@ -48,8 +48,8 @@ int main() {
     double gain = 10;
     double r_wheel_gain = 1.35;
     int avg_val_ctr = 0;
-    int window = 3;
-    double corr_arr[3];
+    int window = 4;
+    double corr_arr[4];
     double corr_factor_avg = 0.0;
     double sum = 0;
 
