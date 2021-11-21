4dea0f0313e6df33d0a7067c47e4e9fd7c520a52
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-21 15:58:49 -0500

    bug fixes to rolling avg function for wide application to individual colours

--- swirlelib.h
+++ swirlelib.h
@@ -441,11 +441,11 @@ double rolling_avg(double *corr_arr, double *corr_factor, double *sum) {
     double corr_factor_avg = 0;
     //double corr_factor = 0;
 
-    sum = sum - corr_arr[avg_val_ctr];
-    corr_arr[avg_val_ctr] = corr_factor;
-    sum = sum + corr_factor;
-    avg_val_ctr = (avg_val_ctr + 1) % WINDOW; //window size
-    corr_factor_avg = sum / WINDOW;
+    *sum = *sum - corr_arr[avg_val_ctr];
+    corr_arr[avg_val_ctr] = *corr_factor;
+    *sum = *sum + *corr_factor;
+    *avg_val_ctr = (*avg_val_ctr + 1) % WINDOW; //window size
+    corr_factor_avg = *sum / WINDOW;
     return corr_factor_avg;
 }
 
