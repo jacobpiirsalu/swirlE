1ee54a37531033e545e6a8b0e762dc8e8bbc7e5a
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-21 03:37:20 -0500

    test speed

--- swirlE.c
+++ swirlE.c
@@ -44,10 +44,10 @@ int main() {
     double leftC_sense = 0.0;
     double rightC_sense = 0.0;
     double corr_factor = 0.0;
-    double max_speed = 0.08*2; //base speed of swirlE
+    double max_speed = 0.08*1.25; //base speed of swirlE
     // - works decently at 0.08*1.5 and window size 3
     double gain = 10;
-    double r_wheel_gain = 1.35;
+    double r_wheel_gain = 1.2;
     int avg_val_ctr = 0;
     int window = 3;
     double corr_arr[3];
