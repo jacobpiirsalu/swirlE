e141fa221b0e82ee6bfa6ce72a3fe754e7dbed3d
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-21 23:34:45 -0500

    test blue code

--- swirlE.c
+++ swirlE.c
@@ -133,7 +133,7 @@ int main() {
 //    rc_usleep(1000000/2.0);
 //    while(!(robot_turn_cw(90,frequency_hz)));
     rc_usleep(1000000/2.0);
-    robot_forward(5,frequency_hz);
+    while(!(robot_move_forward_bullseye(frequency_hz)));
 
     //robot_move_cup_up(frequency_hz);
     while (0) {
