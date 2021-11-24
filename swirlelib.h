2af3c2719f044db4bf74e30a5fae29c2c19b608b
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-23 22:21:38 -0500

    bug fixes

--- swirlelib.h
+++ swirlelib.h
@@ -337,16 +337,16 @@ int robot_turn_ninety_cw(int frequency_hz) {
         // send result
 
         //ch = 7;
-        if (rc_servo_send_pulse_normalized(7, servo_pos*.75) == -1) return -1;
+        if (rc_servo_send_pulse_normalized(7, servo_pos*.5) == -1) return -1;
         counter++;
 
         //ch = 8;
-        if(rc_servo_send_pulse_normalized(8, servo_pos*.75)==-1) return -1;
+        if(rc_servo_send_pulse_normalized(8, servo_pos*.5)==-1) return -1;
 
         // sleep roughly enough to maintain frequency_hz
         rc_usleep(1000000 / frequency_hz);
 
-        if (counter > 22) break;
+        if (counter > 18) break;
 
     }
     return 1;
@@ -385,11 +385,11 @@ int robot_turn_ninety_ccw(int frequency_hz) {
         // send result
 
         //ch = 7;
-        if (rc_servo_send_pulse_normalized(7, servo_pos*.75) == -1) return -1;
+        if (rc_servo_send_pulse_normalized(7, servo_pos*.5) == -1) return -1;
         counter++;
 
         //ch = 8;
-        if(rc_servo_send_pulse_normalized(8, servo_pos*.75)==-1) return -1;
+        if(rc_servo_send_pulse_normalized(8, servo_pos*.5)==-1) return -1;
 
         // sleep roughly enough to maintain frequency_hz
         rc_usleep(1000000 / frequency_hz);
