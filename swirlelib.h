3a7102fac797662cb523eb23cd42fde397d5a076
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-24 00:21:46 -0500

    bug fixes

--- swirlelib.h
+++ swirlelib.h
@@ -345,7 +345,7 @@ int robot_turn_ninety_cw(int frequency_hz) {
         // sleep roughly enough to maintain frequency_hz
         rc_usleep(1000000 / frequency_hz);
 
-        if (counter > 45) break;
+        if (counter > 100) break;
 
     }
     return 1;
@@ -393,7 +393,7 @@ int robot_turn_ninety_ccw(int frequency_hz) {
         // sleep roughly enough to maintain frequency_hz
         rc_usleep(1000000 / frequency_hz);
 
-        if (counter > 20) break;
+        if (counter > 100) break;
 
     }
     return 1;
