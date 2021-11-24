3d36deee15e276b5f76278c6364b2979b1778507
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-23 23:01:16 -0500

    bug fixes

--- swirlE.c
+++ swirlE.c
@@ -173,13 +173,14 @@ int main() {
             rc_servo_send_pulse_normalized(8, 0);
 
             robot_move_forward_bullseye(frequency_hz);
-            rc_usleep(SLEEP_TIME*100);
+            rc_usleep(SLEEP_TIME*10);
             robot_move_cup_down(frequency_hz);
-            rc_usleep(SLEEP_TIME*100);
+            rc_usleep(SLEEP_TIME*10);
+            robot_back(1,frequency_hz);
             //robot_turn_cw(110*2,frequency_hz);
             robot_turn_one_eighty(frequency_hz,1);
 
-            rc_usleep(SLEEP_TIME*100);
+            rc_usleep(SLEEP_TIME*10);
 
             //state = {0, 0, 0, 0}; //set state back to line following
             state[0] = 0;
