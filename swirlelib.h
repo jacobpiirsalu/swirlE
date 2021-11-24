a388d12284067d7621c5ddb7d60f9b32dc02cb75
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-24 02:35:27 -0500

    fixed end of course

--- swirlelib.h
+++ swirlelib.h
@@ -17,7 +17,7 @@
 #define CS_OUT1 2//GPIO3_2, PURPLE -> left colour sensor
 #define CS_OUT2 1//GPIO3_1, BLU -> right colour sensor //swap
 
-#define WINDOW 5 //10 accepted
+#define WINDOW 10 //10 accepted
 
 //colour detection thresholds
 #define TURN_THRESHOLD 0.1
