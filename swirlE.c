0ac3b49a789c0d2e52b1cf2cd52d3d24df087a33
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-23 19:39:07 -0500

    made polling more frequent

--- swirlE.c
+++ swirlE.c
@@ -86,33 +86,33 @@ int main() {
     while(1) {
         loopctr++;
         if (!state[1] && !state[2]) { //line following state X00X
-            rc_usleep(1000);
+            rc_usleep(20);
             double l_red_val = colour_sensor_red(CS_OUT1);
-            rc_usleep(1000);
+            rc_usleep(20);
             double r_red_val = colour_sensor_red(CS_OUT2);
-            rc_usleep(1000);
+            rc_usleep(20);
             l_r_avg = rolling_avg(l_red_arr, &l_red_val, &l_r_sum);
-            rc_usleep(1000);
+            rc_usleep(20);
             r_r_avg = rolling_avg(r_red_arr, &r_red_val, &r_r_sum);
 
-            rc_usleep(1000);
+            rc_usleep(20);
             double l_green_val = colour_sensor_green(CS_OUT1);
-            rc_usleep(1000);
+            rc_usleep(20);
             double r_green_val = colour_sensor_green(CS_OUT2);
-            rc_usleep(1000);
+            rc_usleep(20);
             l_g_avg = rolling_avg(l_green_arr, &l_green_val, &l_g_sum);
-            rc_usleep(1000);
+            rc_usleep(20);
             r_g_avg = rolling_avg(r_green_arr, &r_green_val, &r_g_sum);
-            rc_usleep(1000);
+            rc_usleep(20);
 
             double l_blue_val = colour_sensor_blue(CS_OUT1);
-            rc_usleep(1000);
+            rc_usleep(20);
             double r_blue_val = colour_sensor_blue(CS_OUT2);
-            rc_usleep(1000);
+            rc_usleep(20);
             l_b_avg = rolling_avg(l_blue_arr, &l_blue_val, &l_b_sum);
-            rc_usleep(1000);
+            rc_usleep(20);
             r_b_avg = rolling_avg(r_blue_arr, &r_blue_val, &r_b_sum);
-            rc_usleep(1000);
+            rc_usleep(20);
 
             leftC_sense = l_r_avg + l_g_avg + l_b_avg; //left
 
