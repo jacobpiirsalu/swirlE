86c83f8f372a5a17127ac07976f08b6f9a90c358
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-25 17:17:18 -0500

    mvp changes

--- swirlE.c
+++ swirlE.c
@@ -177,17 +177,17 @@ int main() {
             //printf("\n%f  %f",l_red_val,r_red_val);
 
             //printf("\nsaw red\n");
-            l_r_avg = 2*rolling_avg(l_red_arr, &l_red_val, &l_r_sum);
+            l_r_avg = rolling_avg(l_red_arr, &l_red_val, &l_r_sum);
             //rc_usleep(20);
-            r_r_avg = 2*rolling_avg(r_red_arr, &r_red_val, &r_r_sum);
+            r_r_avg = rolling_avg(r_red_arr, &r_red_val, &r_r_sum);
             //rc_usleep(20);
             double l_green_val = colour_sensor_green(CS_OUT1);
             rc_usleep(20);
             double r_green_val = colour_sensor_green(CS_OUT2);
             rc_usleep(20);
-            l_g_avg = 2*rolling_avg(l_green_arr, &l_green_val, &l_g_sum);
+            l_g_avg = rolling_avg(l_green_arr, &l_green_val, &l_g_sum);
             //rc_usleep(20);
-            r_g_avg = 2*rolling_avg(r_green_arr, &r_green_val, &r_g_sum);
+            r_g_avg = rolling_avg(r_green_arr, &r_green_val, &r_g_sum);
             //rc_usleep(20);
             printf("%f %f\n",l_r_avg,r_r_avg);
 
@@ -195,9 +195,9 @@ int main() {
             rc_usleep(20);
             double r_blue_val = colour_sensor_blue(CS_OUT2);
             rc_usleep(20);
-            l_b_avg = 2*rolling_avg(l_blue_arr, &l_blue_val, &l_b_sum);
+            l_b_avg = rolling_avg(l_blue_arr, &l_blue_val, &l_b_sum);
             rc_usleep(20);
-            r_b_avg = 2*rolling_avg(r_blue_arr, &r_blue_val, &r_b_sum);
+            r_b_avg = rolling_avg(r_blue_arr, &r_blue_val, &r_b_sum);
             rc_usleep(20);
             //printf("\nsaw color\n");
             if(state[0] && !state[1] && !state[2] && !state[3]) { //if going towards bullseye in state 1000
@@ -228,6 +228,12 @@ int main() {
             if (servo_pos > sweep_limit) {
                 servo_pos = sweep_limit;
             }
+            l_r_avg *= 2;
+            r_r_avg *= 2;
+            l_g_avg *= 2;
+            r_g_avg *= 2;
+            l_b_avg *= 2;
+            r_b_avg *= 2;
             //printf("\nturning\n");
             //printf("%f %f\n", l_r_avg, r_r_avg);
             if ((l_r_avg + 300) < LEFT_RED_LINE) { //left sensor greater than right, turn right
