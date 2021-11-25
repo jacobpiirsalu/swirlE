46b9618ec78924adef258031028283c8155031c6
Jacob Piirsalu <jacob.piirsalu@gmail.com>
2021-11-25 01:57:38 -0500

    mvp changes

--- swirlelib.h
+++ swirlelib.h
@@ -403,7 +403,7 @@ int robot_turn_ninety_ccw(int frequency_hz) {
         // sleep roughly enough to maintain frequency_hz
         rc_usleep(1000000 / frequency_hz);
 
-        if (counter > 95) break;
+        if (counter > 98) break;
 
     }
     return 1;
